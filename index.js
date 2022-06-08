be464f50cf9d8ac217437f2c033de47d15ca8e0c
huchun <1575724298@qq.com>
2022-06-08 19:54:42 +0800

    init project

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: suiyue
+ * @email: 1373842098@qq.com
+ * @Date: 2022-06-08 10:40:13
+ * @LastEditors: sj
+ * @LastEditTime: 2022-06-08 10:40:14
+ */
+alert('hi')
